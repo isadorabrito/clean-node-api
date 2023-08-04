4cf78707fc9a6bbfd1eb1d15982de7479cf66012
isadorabrito <isadoradpbrito@gmail.com>
2023-08-04 19:31:46 -0300

    fix: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,7 @@
 import type { Config } from 'jest'
 
-export const config: Config = {
-  roots: ['<rootDir>/src'],
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
+const config: Config = {
+  verbose: true
 }
+
+export default config
